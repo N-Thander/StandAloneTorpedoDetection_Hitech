43a4ecb1fdcc063c7502f86d31f68acf57dfadbb
N-Thander <108251321+N-Thander@users.noreply.github.com>
2024-11-05 10:47:29 +0530

    Components for deployment

---
+++ classifyCode.py
@@ -0,0 +1,29 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath('TorpedoDetectionApp_Hitech')))
+
+from imports import *
+
+def clsTorpedo(clsModelPath, imagePath):
+    clsModel = YOLO(clsModelPath)
+    
+    results = clsModel.predict(source=imagePath)
+    
+    if not results:
+        print("No result from Torpedo Model")
+        return False
+    
+    for result in results:
+        if hasattr(result, 'probs'):
+            top1_class_id = result.probs.top1
+            top1_confidence = result.probs.top1conf
+            
+            s = f"{top1_class_id} {result.names[top1_class_id]} {top1_confidence:.2f}"
+            
+            if "NonTorpedoFrame" in s:
+                return False
+        else:
+            print("Result does not contain probability information")
+    
+    return True
+
